7b19856aa78729cbaf6614f297beac7bc42587ba
cwallant <74073999+cwallant@users.noreply.github.com>
2022-04-07 11:32:33 -0400

    Update twitter.py

--- snscrape/modules/twitter.py
+++ snscrape/modules/twitter.py
@@ -610,6 +610,10 @@ class _TwitterAPIScraper(snscrape.base.Scraper):
 				i = kwargs['tcooutlinks'].index(card.url)
 			except ValueError:
 				_logger.warning('Could not find card URL in tcooutlinks')
+			### ADDED BY ME! START
+    			except KeyError:
+         			_logger.warning('Could not find key tcooutlinks')
+			### ADDED BY ME! END
 			else:
 				card.url = kwargs['outlinks'][i]
 		return Tweet(**kwargs)
@@ -629,6 +633,8 @@ class _TwitterAPIScraper(snscrape.base.Scraper):
 				cardKwargs[kwarg] = value['image_value']['url']
 			else:
 				raise snscrape.base.ScraperError(f'Unknown card value type: {value["type"]!r}')
+		if 'title' not in cardKwargs:
+    			cardKwargs['title'] = 'THIS IS A TWEET LINKING A TWITTER SPACE'
 		return Card(**cardKwargs)
 
 	def _tweet_to_tweet(self, tweet, obj):
